f4627bfb9378dbc768b26ead9e8425136e8f6ba2
bendoh2006 <bendoh2006@gmail.com>
2026-03-13 17:07:14 +0000

    les dossiers

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='Generetive AI project',
+    version='0.0.0',
+    author='Benjamin',
+    author_email='bendoh2006@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
